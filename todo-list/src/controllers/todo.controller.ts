ab21c12ebaf244b8cf3be38efc2ed8756fc654b6
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-24 11:50:15 +0200

    Update todo.controller.ts

--- todo-list/src/controllers/todo.controller.ts
+++ todo-list/src/controllers/todo.controller.ts
@@ -22,26 +22,43 @@ export class TodoController {
     @inject('services.Geocoder') protected geoService: Geocoder,
   ) {}
 
-  @post('/todos')
-  @response(200, {
-    description: 'Todo model instance',
-    content: {'application/json': {schema: getModelSchemaRef(Todo)}},
+//here
+ @post('/todos', {
+    responses: {
+      '200': {
+        description: 'Todo model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Todo)}},
+      },
+    },
   })
   async create(
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(Todo, {
-            title: 'NewTodo',
-            exclude: ['id'],
-          }),
+          schema: getModelSchemaRef(Todo, {title: 'NewTodo', exclude: ['id']}),
         },
       },
     })
     todo: Omit<Todo, 'id'>,
   ): Promise<Todo> {
+    if (todo.remindAtAddress) {
+      const geo = await this.geoService.geocode(todo.remindAtAddress);
+
+      if (!geo[0]) {
+        // address not found
+        throw new HttpErrors.BadRequest(
+          `Address not found: ${todo.remindAtAddress}`,
+        );
+      }
+
+      // Encode the coordinates as "lat,lng" (Google Maps API format). See also
+      // https://stackoverflow.com/q/7309121/69868
+      // https://gis.stackexchange.com/q/7379
+      todo.remindAtGeo = `${geo[0].y},${geo[0].x}`;
+    }
     return this.todoRepository.create(todo);
   }
+//here
 
   @get('/todos/count')
   @response(200, {
